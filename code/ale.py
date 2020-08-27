6630da22638df2d1012f89427923cedbb8b22b00
mriedel56 <miriedel@fiu.edu>
2020-08-27 07:46:01 -0400

    includes ale, rsfc, and macm results

--- code/ale.py
+++ code/ale.py
@@ -8,6 +8,13 @@ from glob import glob
 from nimare.correct import FWECorrector
 from nimare.meta.ale import ALE, ALESubtraction
 
+def thresh_img(logp_img, z_img, p):
+    sig_inds = np.where(logp_img.get_fdata() > -np.log(p))
+    z_img_data = z_img.get_fdata()
+    z_img_thresh_data = np.zeros(z_img.shape)
+    z_img_thresh_data[sig_inds] = z_img_data[sig_inds]
+    z_img = nib.Nifti1Image(z_img_thresh_data, z_img.affine)
+    return z_img
 
 project_dir = '/home/data/nbc/misc-projects/meta-analyses/meta-analysis_ptsd'
 output_dir = op.join(project_dir, 'derivatives', 'ale')
@@ -36,18 +43,10 @@ os.makedirs(output_dir, exist_ok=True)
 
 dset.save(op.join(output_dir, prefix + ".pkl.gz"))
 
-logp_img = nib.load(op.join(output_dir, '{prefix}_logp_level-cluster_corr-FWE_method-montecarlo.nii.gz'.format(prefix=prefix)))
-sig_inds = np.where(logp_img.get_fdata() > -np.log(0.05))
-
+z_img_logp = nib.load(op.join(output_dir, '{prefix}_logp_level-cluster_corr-FWE_method-montecarlo.nii.gz'.format(prefix=prefix)))
 z_img = nib.load(op.join(output_dir, '{prefix}_z.nii.gz'.format(prefix=prefix)))
-z_img_data = z_img.get_fdata()
-
-z_img_thresh_data = np.zeros(z_img.shape)
-z_img_thresh_data[sig_inds] = z_img_data[sig_inds]
-
-z_img = nib.Nifti1Image(z_img_thresh_data, z_img.affine)
-nib.save(z_img, op.join(output_dir, '{prefix}_z_corr-FWE_thresh-05.nii.gz'.format(prefix=prefix)))
-
+z_img_thresh = thresh_img(z_img_logp, z_img, 0.05)
+nib.save(z_img_thresh, op.join(output_dir, '{prefix}_z_corr-FWE_thresh-05.nii.gz'.format(prefix=prefix)))
 
 #Individual meta-analyses and contrast
 dset1 = convert_sleuth_to_dataset(text_files_trauma, target="mni152_2mm")
@@ -59,8 +58,7 @@ ale2 = ALE(kernel__fwhm=None)
 res1 = ale1.fit(dset1)
 res2 = ale2.fit(dset2)
 corr = FWECorrector(
-    method="montecarlo", n_iters=10000, voxel_thresh=0.001, n_cores=4
-)
+    method="montecarlo", n_iters=10000, voxel_thresh=0.001, n_cores=4)
 cres1 = corr.transform(res1)
 cres2 = corr.transform(res2)
 sub = ALESubtraction(n_iters=10000)
@@ -75,14 +73,24 @@ sres.save_maps(output_dir=output_dir, prefix=prefix3)
 dset1.save(op.join(output_dir, prefix1 + ".pkl.gz"))
 dset2.save(op.join(output_dir, prefix2 + ".pkl.gz"))
 
+#get thresholded individual analyses
+z_img_group1_logp  = nib.load(op.join(output_dir, '{prefix1}_logp_level-cluster_corr-FWE_method-montecarlo.nii.gz'.format(prefix1=prefix1)))
+z_img_group1 = nib.load(op.join(output_dir, '{prefix1}_z.nii.gz'.format(prefix1=prefix1)))
+z_img_group1_thresh = thresh_img(z_img_group1_logp, z_img_group1, 0.05)
+nib.save(z_img_group1_thresh, op.join(output_dir, '{prefix1}_z_corr-FWE_thresh-05.nii.gz'.format(prefix1=prefix1)))
+
+z_img_group2_logp  = nib.load(op.join(output_dir, '{prefix2}_logp_level-cluster_corr-FWE_method-montecarlo.nii.gz'.format(prefix2=prefix2)))
+z_img_group2 = nib.load(op.join(output_dir, '{prefix2}_z.nii.gz'.format(prefix2=prefix2)))
+z_img_group2_thresh = thresh_img(z_img_group2_logp, z_img_group2, 0.05)
+nib.save(z_img_group2_thresh, op.join(output_dir, '{prefix2}_z_corr-FWE_thresh-05.nii.gz'.format(prefix2=prefix2)))
+
+#get thresholded contrast analyses
 z_img = nib.load(op.join(output_dir, '{prefix}_z_desc-group1MinusGroup2.nii.gz'.format(prefix=prefix3)))
 
 #for first direction
 sig_inds = np.where(z_img.get_fdata() > -np.log(0.05))
 
-z_img_group1 = nib.load(op.join(output_dir, '{prefix}_z.nii.gz'.format(prefix='TrauEgtPTSD')))
 z_img_group1_data = z_img_group1.get_fdata()
-z_img_group2 = nib.load(op.join(output_dir, '{prefix}_z.nii.gz'.format(prefix='HCgtPTSD')))
 z_img_group2_data = z_img_group2.get_fdata()
 
 #for first direction
@@ -93,7 +101,7 @@ sig_inds = np.where(z_img.get_fdata() < np.log(0.05))
 z_img_group1_thresh_data[sig_inds] = zimg1_sub_zimg2[sig_inds]
 
 z_img_group1_final = nib.Nifti1Image(z_img_group1_thresh_data, z_img_group1.affine)
-nib.save(z_img_group1_final, op.join(output_dir, '{prefix}_z_thresh-05.nii.gz'.format(prefix='TrauEgtPTSD-HCgtPTSD')))
+nib.save(z_img_group1_final, op.join(output_dir, '{prefix1}-{prefix2}_z_thresh-05.nii.gz'.format(prefix1=prefix1, prefix2=prefix2)))
 
 #for second direction
 zimg2_sub_zimg1 = z_img_group2_data - z_img_group1_data
@@ -103,4 +111,4 @@ sig_inds = np.where(z_img.get_fdata() < np.log(0.05))
 z_img_group2_thresh_data[sig_inds] = zimg2_sub_zimg1[sig_inds]
 
 z_img_group2_final = nib.Nifti1Image(z_img_group2_thresh_data, z_img_group2.affine)
-nib.save(z_img_group2_final, op.join(output_dir, '{prefix}_z_thresh-05.nii.gz'.format(prefix='HCgtPTSD-TrauEgtPTSD')))
+nib.save(z_img_group2_final, op.join(output_dir, '{prefix2}-{prefix1}_z_thresh-05.nii.gz'.format(prefix1=prefix1, prefix2=prefix2)))
